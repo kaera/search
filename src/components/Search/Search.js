30cf61300778e79e68b7656188b34bbf87b702e8
Ana <anastasy.d@gmail.com>
2021-06-07 23:24:44 +0200

    Implement search, add unit tests

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,7 +1,48 @@
-export default function Search() {
+import React, { useRef, useState } from "react";
+import "./Search.css";
+
+export default function Search(props) {
+  const [userInput, setUserInput] = useState("");
+  const searchInput = useRef(null);
+
+  const handleChange = (e) => {
+    setUserInput(e.target.value);
+  };
+
+  const handleClearInput = (e) => {
+    e.preventDefault();
+    setUserInput("");
+    searchInput.current.focus();
+  };
+
+  const handleSearch = (e) => {
+    e.preventDefault();
+    props.onSubmit(userInput);
+  };
+
   return (
     <div className="search" data-testid="Search">
-      It's Search!
+      <form className="search-box" onSubmit={handleSearch}>
+        <input
+          value={userInput}
+          name="search"
+          className="input-search"
+          placeholder="Zoeken"
+          onChange={handleChange}
+          autoComplete="off"
+          ref={searchInput}
+        ></input>
+        {userInput ? (
+          <button
+            className="clear-button"
+            type="reset"
+            onClick={handleClearInput}
+          ></button>
+        ) : (
+          <></>
+        )}
+        <button className="search-button" type="submit"></button>
+      </form>
     </div>
   );
 }
--- src/components/Search/Search.test.js
+++ src/components/Search/Search.test.js
@@ -1,12 +1,80 @@
 import React from "react";
-import { render, cleanup } from "@testing-library/react";
+import { render, cleanup, fireEvent } from "@testing-library/react";
 import Search from "./Search";
 import "@testing-library/jest-dom";
+
 describe("Search", () => {
   afterEach(cleanup);
 
   test("it should mount", () => {
     const { getByTestId } = render(<Search />);
     expect(getByTestId("Search")).toBeInTheDocument();
+  });
+
+  test("it should display search button when input is empty and with value", () => {
+    const { container, getByRole } = render(<Search />);
+    const searchButton = container.querySelector(".search-button");
+    const input = getByRole("textbox");
+
+    expect(input.value).toEqual("");
+    expect(searchButton).toBeInTheDocument();
+
+    fireEvent.change(input, { target: { value: "new value" } });
+
+    expect(input.value).toEqual("new value");
+    expect(searchButton).toBeInTheDocument();
+  });
+
+  test("it should have autocomplete = off", () => {
+    const { getByRole } = render(<Search />);
+    const input = getByRole("textbox");
+    expect(input.autocomplete).toEqual("off");
+  });
+  test("it should display clear button when input has value", () => {
+    const { getByRole, container } = render(<Search />);
+    const input = getByRole("textbox");
+    fireEvent.change(input, { target: { value: "new value" } });
+    const clearButton = container.querySelector(".clear-button");
+
+    expect(input.value).toEqual("new value");
+    expect(clearButton).toBeInTheDocument();
+  });
+
+  test("it should not display clear button when input is empty", () => {
+    const { getByRole, container } = render(<Search />);
+    const input = getByRole("textbox");
+    const clearButton = container.querySelector(".clear-button");
+
+    expect(input.value).toEqual("");
+    expect(clearButton).not.toBeInTheDocument();
+  });
+
+  test("it should not display clear button after click on it", () => {
+    const { getByRole, container } = render(<Search />);
+    const input = getByRole("textbox");
+
+    fireEvent.change(input, { target: { value: "new value" } });
+    const clearButton = container.querySelector(".clear-button");
+    expect(input.value).toEqual("new value");
+
+    fireEvent.click(clearButton);
+
+    expect(input.value).toEqual("");
+    expect(clearButton).not.toBeInTheDocument();
+  });
+
+  test("it should search on Enter", () => {
+    const onSubmit = jest.fn();
+    const newInputValue = "new value";
+    const { getByRole, container } = render(<Search onSubmit={onSubmit} />);
+
+    const input = getByRole("textbox");
+    const searchButton = container.querySelector(".search-button");
+
+    fireEvent.change(input, { target: { value: newInputValue } });
+    expect(input.value).toEqual(newInputValue);
+
+    fireEvent.click(searchButton);
+    expect(onSubmit).toHaveBeenCalledWith(newInputValue);
   });
 });
